6a2f45ae96523f89b4aa886b8a280b1eb055df66
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-09-25 13:56:32 +0500

    1-masala

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// 1-Masala
+
+// let a = Number(prompt("a sonini kiriting"));
+// let b = Number(prompt("b sonini kiriting"));
+
+// function sum(a, b) {
+//     let S = a * b;          
+//     let L = 2 * (a + b);    
+
+//     console.log("Yuza:", S);
+//     console.log("Perimetr:", L);
+// }
+
+// sum(a, b);
